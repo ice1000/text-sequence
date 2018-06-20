fe075f85038fab4b3197bf50b015259bcefc6a6d
ice1000 <ice1000kotlin@foxmail.com>
2018-06-21 02:53:15 +0800

    Added functionality

--- common/src/org/ice1000/textseq/TextSequence.java
+++ common/src/org/ice1000/textseq/TextSequence.java
@@ -7,6 +7,8 @@ import org.jetbrains.annotations.NotNull;
  * Mutable version of {@link CharSequence}
  *
  * @author ice1000
+ * @implNote must impl one of {@link TextSequence#delete(int)} or {@link TextSequence#delete(int, int)} and
+ * one of {@link TextSequence#insert(int, char)} or {@link TextSequence#insert(int, CharSequence)}
  * @since v0.1
  */
 public interface TextSequence extends CharSequence {
@@ -17,10 +19,27 @@ public interface TextSequence extends CharSequence {
 		return characters;
 	}
 
-	void insert(int index, char c);
-	void delete(int index);
+	default void insert(int index, char c) {
+		insert(index, String.valueOf(c));
+	}
+
+	default void delete(int index) {
+		delete(index, index + 1);
+	}
+
+	default void insert(int index, @NotNull CharSequence sequence) {
+		for (int i = 0; i < sequence.length(); i++) insert(index + i, sequence.charAt(i));
+	}
+
+	default void delete(int begin, int end) {
+		for (int i = begin; i < end; i++) delete(begin);
+	}
 
 	default void append(char c) {
 		insert(length(), c);
+	}
+
+	default void append(@NotNull CharSequence sequence) {
+		insert(length(), sequence);
 	}
 }
--- common/test/org/ice1000/textseq/TextSequenceTest.java
+++ common/test/org/ice1000/textseq/TextSequenceTest.java
@@ -10,9 +10,7 @@ public class TextSequenceTest {
 	public void main() {
 		TextSequence characters = new ArrayListTextSequence();
 		String s = "reiuji utsuho";
-		for (int i = 0; i < s.length(); i++) {
-			characters.append(s.charAt(i));
-		}
+		for (int i = 0; i < s.length(); i++) characters.append(s.charAt(i));
 		characters.subSequence(3, 5);
 		assertEquals(s.substring(3, 7), characters.subSequence(3, 7).toString());
 	}
@@ -23,6 +21,14 @@ public class TextSequenceTest {
 		characters.append('a');
 		characters.append('b');
 		characters.append('c');
+		assertEquals("abc", characters.toString());
+		assertEquals('a', characters.charAt(0));
+	}
+
+	@Test
+	public void insert() {
+		TextSequence characters = new ArrayListTextSequence();
+		characters.append("abc");
 		assertEquals("abc", characters.toString());
 		assertEquals('a', characters.charAt(0));
 	}
